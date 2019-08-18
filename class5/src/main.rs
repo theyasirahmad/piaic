e1f016db1f94c111ed44d46c368ccdc05150c916
Yasir Ahmed <yasira640@gmail.com>
2019-08-18 18:17:27 +0500

    class 5 almost end

--- class5/src/main.rs
+++ class5/src/main.rs
@@ -43,11 +43,105 @@ fn main() {
 // control flow --  conditional statements
 // if else 
 
-    let x = 10;
-    let y = 15;
+    // let x = 10;
+    // let y = 15;
 
-    if x<y{
-        println!("bela ciao");
+    // if x<y{
+    //     println!("bela ciao");
+    // }
+
+
+    // fn marks_sheet(){
+
+    //     let marks = 50;
+
+    //     if marks>=80{
+    //         println!("A+");
+    //     }
+    //     else if marks>=70 && marks < 80 {
+    //         println!("A");
+    //     }
+    //     else if marks>=60 && marks < 70 {
+    //         println!("B");
+    //     }
+    //     else if marks>=50 && marks < 60 {
+    //         println!("C");
+    //     }
+    //     else{
+    //         println!("fail");
+    //     }
+    // }
+    
+    // marks_sheet();
+
+
+
+    // LOOPS
+
+    // loop 
+    // for
+    // while 
+
+    
+    // loop
+    
+    // let mut x = 0;
+    // loop{
+    //     println!("{}",x );
+    //     x = x + 1;
+
+
+    //     if x>100000{
+    //         break;
+    //     }
+    // }
+
+
+    // for loop
+
+    // for i in 1..100{
+    //  println!("{}",i );
+    // }
+
+    // let fruits = ["apple",  "banana", "mangoes"];
+
+    // for element in fruits.iter(){
+    //     println!("{}",element );
+    // }
+
+    // println!("");
+
+    // let fruits2 = ["apple",  "banana", "mangoes"];
+
+    // for index in 0..3{
+    //     println!("{}",fruits2[index] );
+    // }
+
+    // for in reverse
+
+    // let fruits = ["apple",  "banana", "mangoes"];
+
+    // for element in fruits.iter().rev(){
+    //     println!("{}",element );
+    // }
+
+    // println!("");
+
+    // let fruits2 = ["apple",  "banana", "mangoes"];
+
+    // for index in (0..3).rev(){
+    //     println!("{}",fruits2[index] );
+    //     println!("{}",index );
+    // }
+
+
+    // while loop
+
+    let mut x = 0 ;
+    while x<10{
+        println!("hellos");
+        println!("{}",x );
+        x= x+1 ;
     }
 
 }
